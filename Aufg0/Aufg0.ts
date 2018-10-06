0ee6f3774995f1e7db3378403727b687b7c395b4
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-06 21:17:20 +0900

    Update Aufg0.ts
    got the response working

--- Aufg0/Aufg0.ts
+++ Aufg0/Aufg0.ts
@@ -4,7 +4,7 @@ namespace aufgabe0 {
     function main() {
         let txt = prompt("whats your name?");
         let node : any = document.getElementById("dembody");
-        node.innerHTML += "Hey there";
+        node.innerHTML += "Hey there ";
         node.innerHTML += txt;
         node.innerHTML += ", glad you are with us.";
         console.log(txt);
