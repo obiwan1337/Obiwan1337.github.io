c74ccc0f959b09d2b681d6fc953a59d298aaf192
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-09 03:29:41 +0900

    Add files via upload

---
+++ Aufg0.ts
@@ -0,0 +1,12 @@
+namespace aufgabe0 {
+    function main() {
+      let  input : string = prompt("whats your name?");
+        let txt : any = document.getElementById("dembody");
+        txt += "Hey there  " + input + ", glad you are with us.";
+
+        console.log(input);
+
+        }
+
+    document.addEventListener('DOMContentLoaded',main);
+}
