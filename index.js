73c49b255d9491398de27da7d793e586ebe88852
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-09 06:40:23 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var node = document.getElementById("burg");
 var number = 0;
 while (number < 1) {
-    var htmlnode = "";
-    htmlnode += "<h2> A SMaLL page</h2>";
-    node.innerHTML += htmlnode;
+    var ntxt = "";
+    ntxt += "<h2> A SMaLL page</h2>";
+    document.getElementById("burg").innerHTML += ntxt;
     number++;
 }
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-  var node : any = document.getElementById("burg");
+
   let number: number = 0;
   while (number < 1){
-let htmlnode:string = "";
-htmlnode+= "<h2> A SMaLL page</h2>";
+    let ntxt :string = "";
+    ntxt+= "<h2> A SMaLL page</h2>";
 
-
-node.innerHTML += htmlnode;
+  document.getElementById("burg").innerHTML+= ntxt;
   number++;
 }
