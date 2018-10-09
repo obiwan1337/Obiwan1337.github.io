a949d97f1bc8d3243e17c4455ad80ed983038a7d
obiwan1337 <43846856+obiwan1337@users.noreply.github.com>
2018-10-09 16:33:26 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var number = 0;
+var ntxt = "";
 while (number < 1) {
-    var ntxt = "";
     ntxt += "<h2> A SMaLL page</h2>";
     document.getElementById("burg").innerHTML += ntxt;
     number++;
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 
   let number: number = 0;
+  let ntxt :string = "";
   while (number < 1){
-    let ntxt :string = "";
+
+
     ntxt+= "<h2> A SMaLL page</h2>";
 
   document.getElementById("burg").innerHTML+= ntxt;
